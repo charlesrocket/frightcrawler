084697f11298ac9abc9e2c620afce7f91c895e44
-k <slowdive@me.com>
2021-04-07 19:13:08 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readmefile:
+    readme = readmefile.read()
+
+setup(
+    name='frightcrawler',
+    version='0.0.1',
+    author='charlesrocket',
+    license='MIT',
+    description='',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/charlesrocket/frightcrawler',
+    install_requires=[
+        'dictor>=0.1.6',
+        'requests>=2.25.0',
+    ],
+    python_requires='>=3.6',
+    packages=find_packages(),
+)
