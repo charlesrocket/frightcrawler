e4ab1e91ba462a0a67640e3cebccf1685eb71df0
-k <slowdive@me.com>
2021-04-26 15:54:56 -0700

    Update frightcrawler.cr

--- src/frightcrawler.cr
+++ src/frightcrawler.cr
@@ -19,14 +19,15 @@ module Frightcrawler
   counter = 0
   File.open("helvault.csv") do |infile|
     cardlist = CSV.new(infile, header = true)
-      puts "  Processing CSV file ..."
-      cardlist.each do |entry|
-        row = entry.row.to_a
-        scry_id = "https://api.scryfall.com/cards/" + row[6]
-        card_name = row[3]
-        foil_status = row[1]
-        scry_api = Halite.get("#{scry_id}")
-        sleep 0.1
-      end
+    puts "  Processing CSV file ..."
+    cardlist.each do |entry|
+      row = entry.row.to_a
+      scry_id = "https://api.scryfall.com/cards/" + row[6]
+      card_name = row[3]
+      foil_status = row[1]
+      scry_api = Halite.get("#{scry_id}")
+      puts scry_api
+      sleep 0.1
     end
   end
+end
