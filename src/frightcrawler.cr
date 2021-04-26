8d1f3610524a8c8781bd753023f6876359897fed
-k <slowdive@me.com>
2021-04-26 15:38:48 -0700

    Move to crystal

---
+++ src/frightcrawler.cr
@@ -0,0 +1,32 @@
+require "halite"
+require "json"
+require "csv"
+
+# TODO: Write documentation for `Frightcrawler`
+module Frightcrawler
+  VERSION = "0.0.9"
+
+  intro = "
+  ▓░░░█▀▀░█▀▀▄░░▀░░█▀▀▀░█░░░░▀█▀░
+  ▓░░░█▀░░█▄▄▀░░█▀░█░▀▄░█▀▀█░░█░░
+  ▓░░░▀░░░▀░▀▀░▀▀▀░▀▀▀▀░▀░░▀░░▀░░
+  ▓░█▀▄░█▀▀▄░█▀▀▄░█░░░█░█░░█▀▀░█▀▀▄
+  ▓░█░░░█▄▄▀░█▄▄█░▀▄█▄▀░█░░█▀▀░█▄▄▀
+  ▓░▀▀▀░▀░▀▀░▀░░▀░░▀░▀░░▀▀░▀▀▀░▀░▀▀"
+
+  puts intro
+  loop_count = 1
+  counter = 0
+  File.open("helvault.csv") do |infile|
+    cardlist = CSV.new(infile, header = true)
+      puts "  Processing CSV file ..."
+      cardlist.each do |entry|
+        row = entry.row.to_a
+        scry_id = "https://api.scryfall.com/cards/" + row[6]
+        card_name = row[3]
+        foil_status = row[1]
+        scry_api = Halite.get("#{scry_id}")
+        sleep 0.1
+      end
+    end
+  end
