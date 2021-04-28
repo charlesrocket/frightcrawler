a543aa598211b813959444a57574a4be2ca5b60d
-k <slowdive@me.com>
2021-04-28 08:58:16 -0700

    Update frightcrawler.cr

--- src/frightcrawler.cr
+++ src/frightcrawler.cr
@@ -1,3 +1,4 @@
+require "option_parser"
 require "http"
 require "json"
 require "csv"
@@ -15,58 +16,23 @@ module Frightcrawler
   ▓░▀▀▀░▀░▀▀░▀░░▀░░▀░▀░░▀▀░▀▀▀░▀░▀▀"
 
   puts intro
-  outro = "Help text"
-  if ARGV.size == 0
-    puts outro
-    exit
-  else
-    case ARGV[0]
-    when "brawl"
-      card_format = "brawl"
-    when "commander"
-      card_format = "commander"
-    when "duel"
-      card_format = "duel"
-    when "future"
-      card_format = "future"
-    when "gladiator"
-      card_format = "gladiator"
-    when "historic"
-      card_format = "historic"
-    when "legacy"
-      card_format = "legacy"
-    when "modern"
-      card_format = "modern"
-    when "oldschool"
-      card_format = "oldschool"
-    when "pauper"
-      card_format = "pauper"
-    when "penny"
-      card_format = "penny"
-    when "pioneer"
-      card_format = "pioneer"
-    when "premodern"
-      card_format = "premodern"
-    when "standard"
-      card_format = "standard"
-    when "vintage"
-      card_format = "vintage"
-    when "help", "--help", "-h"
-      puts "help"
+  game_format = "modern"
+  parser = OptionParser.new do |parser|
+    parser.banner = intro
+    parser.on("-g GAME_FORMAT", "Set game format") { |_game_format| game_format = _game_format }
+    parser.on("-h", "--help", "Print this text") do
+      puts parser
       exit
-    when "version", "--version", "-v"
+    end
+    parser.on("-v", "--version", "Version") do
       puts VERSION
       exit
-    else
-      puts "ERROR"
-      exit(1)
     end
   end
-  loop_count = 1
-  counter = 0
+  parser.parse
   File.open("helvault.csv") do |infile|
     cardlist = CSV.new(infile, header = true)
-    puts "  Processing CSV file for #{card_format} format"
+    STDERR.puts "  Processing CSV file for #{game_format} format"
     cardlist.each do |entry|
       row = entry.row.to_a
       scry_id = "https://api.scryfall.com/cards/" + row[6]
@@ -82,12 +48,12 @@ module Frightcrawler
       end
       scry_api = HTTP::Client.get("#{scry_id}")
       api_response = scry_api.body
-      if api_response.includes? %("#{card_format}":"not_legal")
+      if api_response.includes? %("#{game_format}":"not_legal")
         puts "  ▓▒░░░  Not legal  #{foil} #{card_name} ◄ #{set_name} ►"
-      elsif api_response.includes? %("#{card_format}":"legal")
+      elsif api_response.includes? %("#{game_format}":"legal")
         puts "  ▓▒░░░    Legal    #{foil} #{card_name} ◄ #{set_name} ►"
       else
-        puts "ERROR"
+        puts parser
         exit(1)
       end
       sleep 0.1 # API rate limit
