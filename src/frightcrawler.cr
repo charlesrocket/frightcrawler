284076c2d39d049a836b729b54422b8b963637a5
-k <slowdive@me.com>
2021-04-27 23:37:45 -0700

    Update frightcrawler.cr

--- src/frightcrawler.cr
+++ src/frightcrawler.cr
@@ -4,7 +4,7 @@ require "csv"
 
 # TODO: Write documentation for `Frightcrawler`
 module Frightcrawler
-  VERSION = "0.0.9"
+  VERSION = "0.1.0"
 
   intro = "
   ▓░░░█▀▀░█▀▀▄░░▀░░█▀▀▀░█░░░░▀█▀░
@@ -15,20 +15,82 @@ module Frightcrawler
   ▓░▀▀▀░▀░▀▀░▀░░▀░░▀░▀░░▀▀░▀▀▀░▀░▀▀"
 
   puts intro
+  outro = "Help text"
+  if ARGV.size == 0
+    puts outro
+    exit
+  else
+    case ARGV[0]
+    when "brawl"
+      card_format = "brawl"
+    when "commander"
+      card_format = "commander"
+    when "duel"
+      card_format = "duel"
+    when "future"
+      card_format = "future"
+    when "gladiator"
+      card_format = "gladiator"
+    when "historic"
+      card_format = "historic"
+    when "legacy"
+      card_format = "legacy"
+    when "modern"
+      card_format = "modern"
+    when "oldschool"
+      card_format = "oldschool"
+    when "pauper"
+      card_format = "pauper"
+    when "penny"
+      card_format = "penny"
+    when "pioneer"
+      card_format = "pioneer"
+    when "premodern"
+      card_format = "premodern"
+    when "standard"
+      card_format = "standard"
+    when "vintage"
+      card_format = "vintage"
+    when "help", "--help", "-h"
+      puts "help"
+      exit
+    when "version", "--version", "-v"
+      puts VERSION
+      exit
+    else
+      puts "ERROR"
+      exit
+    end
+  end
   loop_count = 1
   counter = 0
   File.open("helvault.csv") do |infile|
     cardlist = CSV.new(infile, header = true)
-    puts "  Processing CSV file ..."
+    puts "  Processing CSV file for #{card_format} format"
     cardlist.each do |entry|
       row = entry.row.to_a
       scry_id = "https://api.scryfall.com/cards/" + row[6]
       card_name = row[3]
       foil_status = row[1]
+      set_name = row[8]
+      if foil_status == '1'
+        foil = "◆"
+      elsif foil_status == "foil"
+        foil = "◆"
+      else
+        foil = "●"
+      end
       scry_api = HTTP::Client.get("#{scry_id}")
       api_response = scry_api.body
-      puts api_response.includes? %("standard":"not_legal")
-      sleep 0.1
+      if api_response.includes? %("#{card_format}":"not_legal")
+        puts "  ▓▒░░░  Not legal  #{foil} #{card_name} ◄ #{set_name} ►"
+      elsif api_response.includes? %("#{card_format}":"legal")
+        puts "  ▓▒░░░    Legal    #{foil} #{card_name} ◄ #{set_name} ►"
+      else
+        puts "ERROR"
+        exit
+      end
+      sleep 0.1 # API rate limit
     end
   end
 end
