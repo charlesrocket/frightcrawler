ef69d14802b33aeea9c3a5a272f74000a320f489
-k <slowdive@me.com>
2021-05-05 12:24:51 -0700

    Update bulk.cr

--- src/bulk.cr
+++ src/bulk.cr
@@ -4,7 +4,7 @@ require "json"
 def pull_bulk
   bulk_api = HTTP::Client.get("https://api.scryfall.com/bulk-data")
   bulk_json = JSON.parse("#{bulk_api.body}")
-  download_link = bulk_json["data"][2]["download_uri"]
+  download_link = bulk_json["data"][3]["download_uri"]
   if !File.exists?("bulk-data.json")
     puts "\n  Downloading bulk data from Scryfall..."
     HTTP::Client.get("#{download_link}") do |response|
