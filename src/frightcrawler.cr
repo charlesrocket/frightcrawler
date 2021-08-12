90ac4fe16aeab1d5f54dd4cfc8b58346a82cd48e
-k <slowdive@me.com>
2021-08-11 22:20:22 -0700

    Bulk data check
    Make sure bulk data is always fresh

--- src/frightcrawler.cr
+++ src/frightcrawler.cr
@@ -9,6 +9,15 @@ def pull_bulk
   bulk_api = HTTP::Client.get("https://api.scryfall.com/bulk-data")
   bulk_json = JSON.parse("#{bulk_api.body}")
   download_link = bulk_json["data"][3]["download_uri"]
+  if File.exists?("bulk-data.json")
+    local_time = Time.utc.to_unix
+    modification_time = File.info("bulk-data.json").modification_time.to_unix
+    bulk_time = 86000
+    if (local_time - modification_time) >= bulk_time
+      puts "\n  Deleting old bulk data"
+      File.delete("bulk-data.json")
+    end
+  end
   if !File.exists?("bulk-data.json")
     puts "\n  Downloading bulk data from Scryfall ..."
     HTTP::Client.get("#{download_link}") do |response|
