8fe591cbcc8d38984975a180930a3aece03a384a
-k <slowdive@me.com>
2021-10-20 17:13:34 -0700

    Add tests

--- spec/frightcrawler_spec.cr
+++ spec/frightcrawler_spec.cr
@@ -1,9 +1,29 @@
 require "./spec_helper"
 
-describe Crawler do
-  # TODO: Write tests
+describe Bulk, tags: "api" do
+  describe "#pull" do
+    it "downloads bulk data" do
+      Bulk.pull
+      File.exists?("bulk-data.json").should be_true
+    end
+  end
+end
 
-  it "works" do
-    false.should eq(true)
+describe Counter do
+  describe "#get_total" do
+    it "counts totals" do
+      Counter.total(2)
+      Counter.total(3)
+      Counter.get_total.should eq(5)
+    end
+  end
+
+  describe "#get_unique" do
+    it "counts uniques" do
+      Counter.unique
+      Counter.unique
+      Counter.unique
+      Counter.get_unique.should eq(3)
+    end
   end
 end
