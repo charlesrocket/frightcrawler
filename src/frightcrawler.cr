0ef807ac0bc31ed7e5c0c66071bd7c45def47f03
-k <slowdive@me.com>
2021-10-20 17:13:13 -0700

    More refactoring

--- src/frightcrawler.cr
+++ src/frightcrawler.cr
@@ -1,14 +1,12 @@
 require "./bulk"
 require "./counter"
+require "./crawler"
 require "http/client"
 require "option_parser"
 require "colorize"
 require "json"
 require "csv"
 require "log"
-
-class NoData < Exception
-end
 
 VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
 backend = Log::IOBackend.new(File.new("./frightcrawler.log", "a+"))
@@ -40,8 +38,11 @@ OptionParser.parse do |parser|
   end
 end
 
-puts "\n  * Using #{game_format} format list"
-Bulk.pull
+if game_format != ""
+  puts "\n  * Using #{game_format} format list"
+  Bulk.pull
+end
+
 t1 = Time.monotonic
 
 unless sf_id.empty?
@@ -51,121 +52,18 @@ unless sf_id.empty?
 end
 
 if csv_file != Nil
-  Crawler.lineoutput("#{game_format}", "#{csv_file}")
-end
-
-struct Crawler
-  def self.lineoutput(c, f)
-    File.open(f) do |file|
-      cardlist = CSV.new(file, headers: true)
-      csv_header = cardlist.headers.to_s
-      if csv_header.includes? %("extras", "language", "name", "quantity", "scryfall_id")
-        csv_helvault = true
-        puts "\n  * Helvault CSV file loaded"
-      elsif csv_header.includes? %("collector_number", "estimated_price", "extras", "language")
-        csv_helvaultpro = true
-        puts "\n  * Helvault Pro CSV file loaded"
-      elsif csv_header.includes? %(AetherHub Card Id)
-        csv_aetherhub = true
-        puts "\n  * AetherHub CSV file loaded"
-      else
-        raise "Unsupported CSV layout"
-      end
-      puts "\n  * Loading bulk data ..."
-      bulk_json = JSON.parse(File.read("bulk-data.json"))
-      puts "\n  * Bulk data loaded"
-      puts "\n  * Reading CSV file ...", "\n"
-      cardlist.each do |entry|
-        row = entry.row.to_a
-        x = 0
-        case
-        when csv_helvault
-          scry_id = row[4]
-          foil_status = row[0]
-          quantity = row[3]
-        when csv_helvaultpro
-          scry_id = row[8]
-          foil_status = row[2]
-          quantity = row[6]
-        when csv_aetherhub
-          scry_id = row[13]
-          foil_status = row[7]
-          quantity = row[6]
-        else
-          raise "ERROR: csv"
-        end
-        until bulk_json[x]["id"] == "#{scry_id}"
-          # OPTIMIZE: Not good enough!
-          x += 1
-        end
-        scry_json = bulk_json[x]
-        card_name = scry_json["name"]
-        set_name = scry_json["set_name"]
-        set_code = scry_json["set"].to_s.upcase.colorize.mode(:underline)
-        case
-        when foil_status == "1", foil_status == "foil"
-          foil_layout = :▲.colorize(:light_gray)
-          Counter.foil("#{quantity}".to_i)
-        when foil_status == "etchedFoil"
-          foil_layout = :◭.colorize(:light_gray)
-          Counter.efoil("#{quantity}".to_i)
-        when foil_status == "0", foil_status == ""
-          foil_layout = :△.colorize(:dark_gray)
-        else
-          raise "ERROR: foil_status"
-        end
-        case
-        when scry_json["legalities"][c] == "legal"
-          legalities = "  Legal   ".colorize(:green)
-          Counter.legal("#{quantity}".to_i)
-        when scry_json["legalities"][c] == "not_legal"
-          legalities = "Not legal ".colorize(:red)
-          Counter.not_legal("#{quantity}".to_i)
-        when scry_json["legalities"][c] == "restricted"
-          legalities = "  Restr   ".colorize(:blue)
-          Counter.restricted("#{quantity}".to_i)
-        when scry_json["legalities"][c] == "banned"
-          legalities = "   BAN    ".colorize(:red)
-          Counter.banned("#{quantity}".to_i)
-        else
-          raise "ERROR: legalities"
-        end
-        case
-        when scry_json["rarity"] == "common"
-          rarity_symbol = :C.colorize(:white)
-          Counter.common("#{quantity}".to_i)
-        when scry_json["rarity"] == "uncommon"
-          rarity_symbol = :U.colorize(:cyan)
-          Counter.uncommon("#{quantity}".to_i)
-        when scry_json["rarity"] == "rare"
-          rarity_symbol = :R.colorize(:light_yellow)
-          Counter.rare("#{quantity}".to_i)
-        when scry_json["rarity"] == "special"
-          rarity_symbol = :S.colorize(:yellow)
-          Counter.special("#{quantity}".to_i)
-        when scry_json["rarity"] == "mythic"
-          rarity_symbol = :M.colorize(:magenta)
-          Counter.mythic("#{quantity}".to_i)
-        when scry_json["rarity"] == "bonus"
-          rarity_symbol = :B.colorize(:light_blue)
-          Counter.bonus("#{quantity}".to_i)
-        else
-          raise "ERROR: rarity"
-        end
-        Counter.total("#{quantity}".to_i)
-        Counter.unique
-        # TODO: Add icons
-        puts "▓▒░░░  #{legalities} #{foil_layout} #{rarity_symbol} #{card_name} ⬡ #{set_name} ◄ #{set_code} ►"
-        # TODO: Improve logging
-        Log.info { "#{c}: #{legalities} #{card_name} ◄ #{set_name} ► ⑇ #{quantity}" }
-      end
-    end
-  end
+  Crawler.print_line("#{game_format}", "#{csv_file}")
 end
 
 t2 = Time.monotonic
 elapsed_time = t2 - t1
-Log.info { "Processed: #{Counter.get_unique}/#{Counter.get_total}" }
-Counter.output
+
+if csv_file != Nil
+  Log.info { "Processed: #{Counter.get_unique}/#{Counter.get_total}" }
+  Counter.output
+else
+  puts "\n  No data provided"
+end
+
 puts "  Elapsed time: #{elapsed_time}"
 puts "\n  DONE"
