d92b89b24c789cb33d93865fcb4ba7ad814f91e8
-k <slowdive@me.com>
2021-10-25 10:38:20 -0700

    Define get_json and prepare

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,3 +4,17 @@ require "../src/frightcrawler"
 def reset
   Counter.reset
 end
+
+def prepare
+  Bulk.pull
+end
+
+def get_json
+  x = 0
+  prepare
+  bulk_json = JSON.parse(File.read("bulk-data.json"))
+  until bulk_json[x]["id"] == "bd1751ca-4945-4071-87f1-9d5f282c35f0"
+    x += 1
+  end
+  json = bulk_json[x]
+end
