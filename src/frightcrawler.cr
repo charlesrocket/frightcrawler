646691dcd32998b2c788f52c2c04fd52450c0207
-k <slowdive@me.com>
2021-12-08 14:26:01 -0800

    Comment formats

--- src/frightcrawler.cr
+++ src/frightcrawler.cr
@@ -16,7 +16,7 @@ VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
 backend = Log::IOBackend.new(File.new("./frightcrawler.log", "a+"))
 Log.setup(:info, backend)
 
-# :nodoc:
+# Supported formats.
 FORMATS = ["standard", "future", "historic", "gladiator", "pioneer", "modern", "legacy", "pauper", "vintage", "penny", "commander", "brawl", "historicbrawl", "paupercommander", "duel", "oldschool", "premodern"]
 
 # :nodoc:
