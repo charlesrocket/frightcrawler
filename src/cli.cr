871ee3f6077f73e573b47d974dae38a8cb12dc08
-k <slowdive@me.com>
2021-12-09 03:17:39 -0800

    Drop extra flag

--- src/cli.cr
+++ src/cli.cr
@@ -10,7 +10,7 @@ module CLI
       parser.on("-f CSV_FILE", "Path to CSV file") { |_csv_file| csv_file = _csv_file }
       parser.on("-g GAME_FORMAT", "Set game format") { |_game_format_in| game_format_in = _game_format_in }
       parser.on("-i SCRYFALL_ID", "Get card info") { |_sf_id| sf_id = _sf_id }
-      parser.on("-p SPEED", "--speed=SPEED", "Change speed (slow/fast)") { |_speed| speed = _speed }
+      parser.on("-p SPEED", "Change speed (slow/fast)") { |_speed| speed = _speed }
       parser.on("-s", "--sync", "Sync DB") { Database.sync }
       parser.on("-h", "--help", "Print documentation") do
         parser.banner = "Usage: frightcrawler -g modern -f PATH/TO/FILE"
