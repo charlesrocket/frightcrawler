3ea0927ee289920514db021991901ce855e99f17
-k <slowdive@me.com>
2021-12-10 05:09:10 -0800

    Define delay type

--- src/engine.cr
+++ src/engine.cr
@@ -160,7 +160,7 @@ module Engine
   end
 
   # Sets output delay.
-  def self.delay(speed : String = "normal")
+  def self.delay(speed : String = "normal") : Nil
     case speed
     when "slow"
       sleep 0.1
