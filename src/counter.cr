9b98020d7c530c5c592820db56396544b14c0e9c
-k <slowdive@me.com>
2022-01-13 11:20:48 -0800

    Switch to `UInt16`

--- src/counter.cr
+++ src/counter.cr
@@ -1,21 +1,21 @@
 # Tracks validated data's types and count.
 class Counter
-  @@total_count : Int16 = 0
-  @@unique_count : Int16 = 0
+  @@total_count : UInt16 = 0
+  @@unique_count : UInt16 = 0
 
-  @@legal : Int16 = 0
-  @@not_legal : Int16 = 0
-  @@restricted : Int16 = 0
-  @@banned : Int16 = 0
+  @@legal : UInt16 = 0
+  @@not_legal : UInt16 = 0
+  @@restricted : UInt16 = 0
+  @@banned : UInt16 = 0
 
-  @@common : Int16 = 0
-  @@uncommon : Int16 = 0
-  @@rare : Int16 = 0
-  @@special : Int16 = 0
-  @@mythic : Int16 = 0
-  @@bonus : Int16 = 0
-  @@foil : Int16 = 0
-  @@efoil : Int16 = 0
+  @@common : UInt16 = 0
+  @@uncommon : UInt16 = 0
+  @@rare : UInt16 = 0
+  @@special : UInt16 = 0
+  @@mythic : UInt16 = 0
+  @@bonus : UInt16 = 0
+  @@foil : UInt16 = 0
+  @@efoil : UInt16 = 0
 
   # Prints out validation summary.
   def self.output : Nil
@@ -36,115 +36,115 @@ class Counter
     puts "\nDONE"
   end
 
-  def self.total(x) : Int16
+  def self.total(x) : UInt16
     @@total_count += x
   end
 
-  def self.unique : Int16
+  def self.unique : UInt16
     @@unique_count += 1
   end
 
-  def self.legal(x) : Int16
+  def self.legal(x) : UInt16
     @@legal += x
   end
 
-  def self.not_legal(x) : Int16
+  def self.not_legal(x) : UInt16
     @@not_legal += x
   end
 
-  def self.restricted(x) : Int16
+  def self.restricted(x) : UInt16
     @@restricted += x
   end
 
-  def self.banned(x) : Int16
+  def self.banned(x) : UInt16
     @@banned += x
   end
 
-  def self.common(x) : Int16
+  def self.common(x) : UInt16
     @@common += x
   end
 
-  def self.uncommon(x) : Int16
+  def self.uncommon(x) : UInt16
     @@uncommon += x
   end
 
-  def self.rare(x) : Int16
+  def self.rare(x) : UInt16
     @@rare += x
   end
 
-  def self.special(x) : Int16
+  def self.special(x) : UInt16
     @@special += x
   end
 
-  def self.mythic(x) : Int16
+  def self.mythic(x) : UInt16
     @@mythic += x
   end
 
-  def self.bonus(x) : Int16
+  def self.bonus(x) : UInt16
     @@bonus += x
   end
 
-  def self.foil(x) : Int16
+  def self.foil(x) : UInt16
     @@foil += x
   end
 
-  def self.efoil(x) : Int16
+  def self.efoil(x) : UInt16
     @@efoil += x
   end
 
-  def self.get_total : Int16
+  def self.get_total : UInt16
     @@total_count
   end
 
-  def self.get_unique : Int16
+  def self.get_unique : UInt16
     @@unique_count
   end
 
-  def self.get_legal : Int16
+  def self.get_legal : UInt16
     @@legal
   end
 
-  def self.get_not_legal : Int16
+  def self.get_not_legal : UInt16
     @@not_legal
   end
 
-  def self.get_restricted : Int16
+  def self.get_restricted : UInt16
     @@restricted
   end
 
-  def self.get_banned : Int16
+  def self.get_banned : UInt16
     @@banned
   end
 
-  def self.get_common : Int16
+  def self.get_common : UInt16
     @@common
   end
 
-  def self.get_uncommon : Int16
+  def self.get_uncommon : UInt16
     @@uncommon
   end
 
-  def self.get_rare : Int16
+  def self.get_rare : UInt16
     @@rare
   end
 
-  def self.get_special : Int16
+  def self.get_special : UInt16
     @@special
   end
 
-  def self.get_mythic : Int16
+  def self.get_mythic : UInt16
     @@mythic
   end
 
-  def self.get_bonus : Int16
+  def self.get_bonus : UInt16
     @@bonus
   end
 
-  def self.get_foil : Int16
+  def self.get_foil : UInt16
     @@foil
   end
 
-  def self.get_efoil : Int16
+  def self.get_efoil : UInt16
     @@efoil
   end
 
