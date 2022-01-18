5c97115f1d4c07ccb580e430f4aea8a983498e91
-k <slowdive@me.com>
2022-01-17 20:59:45 -0800

    Add `Fixtures`

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,6 +2,12 @@ require "spec"
 require "webmock"
 require "../src/frightcrawler"
 
+struct Fixtures
+  BULK  = File.read("spec/fixtures/bulk.json")
+  CARD  = File.read("spec/fixtures/card.json")
+  CARDS = JSON.parse(File.read("spec/fixtures/cards.json")).to_json
+end
+
 def reset
   Counter.reset
 end
