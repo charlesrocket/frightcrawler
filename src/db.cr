d83fc3ad7c6f387bd7bde478b66142c7163bf452
-k <slowdive@me.com>
2022-04-11 10:11:52 -0700

    Compact `update`

--- src/db.cr
+++ src/db.cr
@@ -126,19 +126,25 @@ module Database
     puts "\n  * Database synchronization ..."
 
     private insert_sql = <<-SQL
-    INSERT OR IGNORE INTO "cards" ("id", "name", "set_name", "set_code", "rarity", "legality_standard", "legality_future",
-                         "legality_historic", "legality_gladiator", "legality_pioneer", "legality_modern",
-                         "legality_legacy", "legality_pauper", "legality_vintage", "legality_penny", "legality_commander",
-                         "legality_brawl", "legality_historicbrawl", "legality_alchemy", "legality_paupercommander", "legality_duel",
+    INSERT OR IGNORE INTO "cards" ("id", "name", "set_name", "set_code", "rarity",
+                         "legality_standard", "legality_future", "legality_historic",
+                         "legality_gladiator", "legality_pioneer", "legality_modern",
+                         "legality_legacy", "legality_pauper", "legality_vintage",
+                         "legality_penny", "legality_commander", "legality_brawl",
+                         "legality_historicbrawl", "legality_alchemy",
+                         "legality_paupercommander", "legality_duel",
                          "legality_oldschool", "legality_premodern")
     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
     SQL
 
     DB.open "sqlite3://#{DB_FILE}" do |db|
-      db.exec "create table if not exists cards (id text primary key, name text, set_name text, set_code text, rarity text, legality_standard text,
-                        legality_future text, legality_historic text, legality_gladiator text, legality_pioneer text, legality_modern text,
-                        legality_legacy text, legality_pauper text, legality_vintage text, legality_penny text, legality_commander text,
-                        legality_brawl text, legality_historicbrawl text, legality_alchemy text, legality_paupercommander text, legality_duel text,
+      db.exec "create table if not exists cards (id text primary key, name text,
+                        set_name text, set_code text, rarity text, legality_standard text,
+                        legality_future text, legality_historic text, legality_gladiator text,
+                        legality_pioneer text, legality_modern text, legality_legacy text,
+                        legality_pauper text, legality_vintage text, legality_penny text,
+                        legality_commander text, legality_brawl text, legality_historicbrawl text,
+                        legality_alchemy text, legality_paupercommander text, legality_duel text,
                         legality_oldschool text, legality_premodern text, timestamp datetime)"
       db.exec "BEGIN TRANSACTION;"
 
